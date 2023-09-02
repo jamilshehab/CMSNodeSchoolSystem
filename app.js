6e35c431d1330ae3e7cdd713eaba2704da5c7512
jamilshehab <shehabjamil20@gmail.com>
2023-09-02 21:14:20 +0300

    Files Added

---
+++ app.js
@@ -0,0 +1,22 @@
+//Imports 
+const express=require('express')
+const app=express()
+const port=3000
+
+
+//Static Files 
+app.use(express.static('public'))
+app.use('/css',express.static(__dirname + 'public/css'))
+app.use('/js',express.static(__dirname + 'public/js'))
+app.use('/images',express.static(__dirname + 'public/images'))
+
+//set views 
+app.set('views' , './views')
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    res.render('login')
+})
+
+//Listen on port 3000
+app.listen(port,()=>console.info(`Listening on port ${port}`))
